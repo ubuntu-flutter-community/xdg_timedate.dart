f5218ac7fcf2712acfdd53b5499c01aeae9a7f1c
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-10-10 23:13:10 +0200

    Add an example

--- example/linux_datetime_service_example.dart
+++ example/linux_datetime_service_example.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:linux_datetime_service/linux_datetime.dart';
+
+void main() async {
+  final dateTimeService = DateTimeService();
+  await dateTimeService.init();
+  print(dateTimeService.dateTime);
+  print(dateTimeService.timezone);
+  await dateTimeService.dispose();
+}
